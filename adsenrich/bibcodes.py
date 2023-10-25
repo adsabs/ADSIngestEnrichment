992c59e59a34663827116331b2b069bcf9f8788c
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-10-25 09:57:24 -0400

    Fixed: minor issues to fix misc issues (IOP)  	modified:   adsenrich/bibcodes.py  	modified:   adsenrich/data.py

--- adsenrich/bibcodes.py
+++ adsenrich/bibcodes.py
@@ -36,7 +36,7 @@ class BibcodeGenerator(object):
 
     def _get_author_init(self, record):
         try:
-            author_init = record['authors'][0]['name']['surname'][0]
+            author_init = record['authors'][0]['name']['surname'].strip().[0]
             author_init = u2asc(author_init).upper()
         except Exception as err:
             author_init = '.'
